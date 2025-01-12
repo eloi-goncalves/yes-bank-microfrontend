be21672110bb377e3177e90766ac33c46addae94
Eloi Gonçalves <eloi.goncalves@philips.com>
2025-01-12 13:13:54 -0300

    Readme configuration

--- home/next.config.js
+++ home/next.config.js
@@ -1,6 +1,5 @@
 const NextFederationPlugin = require('@module-federation/nextjs-mf');
-// this enables you to use import() and the webpack parser
-// loading remotes on demand, not ideal for SSR
+
 const remotes = isServer => {
   const location = isServer ? 'ssr' : 'chunks';
   return {
